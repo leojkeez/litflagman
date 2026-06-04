d9fbe073881ab8e575153320cbceecbc83756903
root <root@aqrfylvhja.local>
2026-06-04 10:33:34 +0000

    Update .gitignore and Site models

--- Site/models.py
+++ Site/models.py
@@ -2,8 +2,8 @@ from django.db import models
 import datetime
 from django.utils.text import slugify
 from django.utils.safestring import mark_safe
-from pytils.translit import slugify as pytils_slugify # Импортируем слаггер для кириллицы
-from ckeditor.fields import RichTextField # или откуда у вас RichTextField
+from pytils.translit import slugify as pytils_slugify
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 
